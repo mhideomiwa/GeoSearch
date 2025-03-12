13d720ceea9215cffb2a3c5ec1721ae4802a7126
matthewmiwa <mhmiwa@miwa.us>
2025-03-12 07:57:37 -0600

    First Commit

---
+++ src/app/OnePlayer/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            One Player
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/app/TwoPlayer/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+import Map from '../components/map';
+
+const Page = () => {
+    return (
+        <div>
+            Two Player
+            <Map/>
+        </div>
+    );
+};
+
+export default Page;
